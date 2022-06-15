cbeccaf51a57a4abb6a810b45627d4d2e5df3575
simon040102 <simon040102@gmail.com>
2022-06-15 13:13:40 +0800

    debug

--- all.js
+++ all.js
@@ -1,31 +1,33 @@
 let text = document.querySelector('.text');
 let save = document.querySelector('.save');
 let list = document.querySelector('.list');
+let check = document.querySelector('.check');
 let ary = [
   {
     item: '今天天氣真好，準備去踏青',
-    finished: 'finished',
-    check: 'confirm-finished',
+    finished: '',
+    check: 'false',
   },
   {
     item: '生人無求，在家耍廢救世界',
     finished: '',
-    check: '',
+    check: 'false',
   },
   {
     item: '拳拳到肉，處處醬肉',
     finished: '',
-    check: '',
+    check: 'false',
   },
   {
     item: '快點寫作業，老師在你後面很火',
-    finished: 'finished',
-    check: 'confirm-finished',
+    finished: '',
+    check: 'false',
   },
   {
     item: '明天的力氣，今天幫你船便便',
     finished: '',
-    check: '',
+    check: 'false',
+
   },
 ];
 init();
@@ -33,14 +35,14 @@ init();
 function init() {
   let str = '';
   ary.forEach(function (items, index) {
-    str += `<li class="${items.finished}"><a  href="#"><img class="check ${items.check}"  data-num=${index} src="images/check_box_outline_blank_black_24dp.svg"></a>${items.item}<a  href="#"><img class="del" data-num=${index} src="images/delete_black_24dp.svg"></a></li>`;
+    str += `<li class="${items.del}"><a  href="#"><img data-check="${items.check}" class="check ${items.finished}"  data-num=${index} src="images/check_box_outline_blank_black_24dp.svg"></a>${items.item}<a  data-num=${index} href="#" class="del"></a></li>`;
   });
   list.innerHTML = str;
 }
 //新增
 save.addEventListener('click', saveData);
 function saveData(e) {
-  let obj = { finished: '' };
+  let obj = { finished: '', check: 'false' ,del:''};
   if (text.value == '') {
     return;
   }
@@ -49,22 +51,32 @@ function saveData(e) {
   init();
   text.value = '';
 }
-//刪除
+//刪除&修改
 list.addEventListener('click', remove);
 function remove(e) {
   let num = e.target.dataset.num;
-  if (e.target.nodeName !== 'IMG') {
-    return
-  }
-  if (e.target.className == 'del') {
+  let check = e.target.dataset.check;
+  if (e.target.nodeName == 'A') {
     ary.splice(num, 1);
+  }
+  if (e.target.nodeName == 'IMG' && e.target.nodeName !== 'A') {
+    console.log(check);
+    console.log(num);
+    if (check == 'false') {
+      ary[num].check = 'true';
+      ary[num].finished = 'confirm-finished';
+      ary[num].del = 'finished';
+    }
+    else if(check == 'true'){
+      ary[num].check = 'false';
+      ary[num].finished = '';
+      ary[num].del = '';
+    }
   }
   init();
 }
 
-
 //checkbox
-
 
 // let check=document.querySelector('.confirm')
 // check.addEventListener('click',checkbox)
